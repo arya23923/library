535f69db09b102246789a82238a2ef816fe85fb7
Arya Gupta <aryagupta239@gmail.com>
2024-07-04 21:51:50 +0530

    modified front end

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
-const books = ['The Kite Runner','First they killed my father','Pachinko','Silent Honor','Tea Girl of Hummingbird Lane'];
-const authors = ['Khaled Hosseini','Loung Ung','Min Jin Lee','Danielle Steel','Lisa See'];
-const year = [2003,2000,2017,1996,2017];
-const rating = [5,4,4,5,5];
+// const library = [
+//     {
+//         title: 'The Kite Runner',
+//         author: 'Khaled Hosseini',
+//         pages: 371,
+//         rating: 5
+//     },
+//     {
+//         title: 'First they killed my father',
+//         author: 'Loung Ung',
+//         pages: 238,
+//         rating: 4
+//     },
+//     {
+//         title: 'Pachinko',
+//         author: 'Min Jin Lee',
+//         pages: 490,
+//         rating: 5
+//     },
+//     {
+//         title: 'Silent Honor',
+//         author: 'Danielle Steel',
+//         pages: 353,
+//         rating: 5
+//     },
+//     {
+//         title: 'Tea Girl of Hummingbird Lane',
+//         author: 'Lisa See',
+//         pages: 384,
+//         rating: 5
+//     }
+// ]
+
+// function Book(title,author,pages,rating){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.rating = rating;
+// }
+
+// function addBookToLibrary(book){
+//     library.push(book);
+// }
+
+// let new_card = document.createElement('div class = card');
+
 
 const add = document.getElementById('Add');
 const dialog = document.getElementById('dialog');
@@ -11,17 +53,34 @@ add.addEventListener("click", ()=>{
     dialog.showModal();
 });
 
-close.addEventListener("click", ()=> {
-    e.preventDefault();
-
-    let book = document.getElementById('book');
-    let author = document.getElementById('author');
-    let years = document.getElementById('years');
-    let ratings = document.getElementById('ratings');
-    let yes = document.getElementById('yes');
-
-    let book_section = document.querySelector('book-section');
-    
+close.addEventListener("click", (event)=> {
+    event.preventDefault();
     dialog.close();
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    let pages = document.getElementById('pages');
+    let rating = document.getElementById('rating');
+
+    let new_card = document.createElement('div class = card');
+    let card_heading = document.createElement('div class = card_heading');
+    let card_writer = document.createElement('div class = writer');
+    let page = document.createElement('div class = page');
+    let ratings = document.createElement('div class = rating');
+
+    card_heading.innerText = title.value;
+    card_writer.innerText = author.value;
+    page.innerText = pages.value+" pages";
+    ratings.innerText = rating.value;
+
+    new_card.appendChild(card_heading);
+    new_card.appendChild(card_writer);
+    new_card.appendChild(page);
+    new_card.appendChild(ratings);
+
+    let book_section = document.getElementsByClassName('book-section').classList;
+    book_section.add("card");
+    book_section.appendChild(new_card);
+
+    
 });
 
